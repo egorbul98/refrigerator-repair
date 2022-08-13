9c99e56c61eb0cd8f9146376fb5986855472ff95
Egor Bulahtin <bulahtiney@m2.ru>
2022-08-13 20:01:38 +0300

    fix: map

--- src/blocks/map-block/map-block.tsx
+++ src/blocks/map-block/map-block.tsx
@@ -3,6 +3,7 @@ import styles from './map-block.module.scss';
 import { YMaps, Map, Placemark } from 'react-yandex-maps';
 import { ADDRESS, MASTER_EMAIL, MASTER_PHONE } from '../../components/feedback-form/constants';
 import { Anchor } from '../../components/anchor/anchor';
+import { Container } from '../../components/container/container';
 
 // const listData = [];
 
@@ -14,18 +15,20 @@ export const MapBlock = () => {
         <>
             <Anchor id="contacts" offset={false} />
             <div className={styles.container}>
-                <div className={styles.contacts}>
-                    <h2 className={styles.title}>Контакты</h2>
-                    <div className={styles.field}>
-                        <b>Адрес:</b> {ADDRESS}
+                <Container className={styles.innerContainer}>
+                    <div className={styles.contacts}>
+                        <h2 className={styles.title}>Контакты</h2>
+                        <div className={styles.field}>
+                            <b>Адрес:</b> {ADDRESS}
+                        </div>
+                        <div className={styles.field}>
+                            <b>Телефон:</b> <a href={`tel:${MASTER_PHONE}`}>{MASTER_PHONE}</a>
+                        </div>
+                        <div className={styles.field}>
+                            <b>E-mail:</b> <a href={`mailto:${MASTER_EMAIL}`}>{MASTER_EMAIL}</a>
+                        </div>
                     </div>
-                    <div className={styles.field}>
-                        <b>Телефон:</b> <a href={`tel:${MASTER_PHONE}`}>{MASTER_PHONE}</a>
-                    </div>
-                    <div className={styles.field}>
-                        <b>E-mail:</b> <a href={`mailto:${MASTER_EMAIL}`}>{MASTER_EMAIL}</a>
-                    </div>
-                </div>
+                </Container>
 
                 <YMaps width={'100%'} height={'100%'}>
                     <Map
